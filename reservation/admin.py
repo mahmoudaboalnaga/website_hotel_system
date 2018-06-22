4dd38e5035969bf33a80f136f5989398ae00a0fa
mahmoud <mahmoud.aboalnaga@hotmail.com>
2018-06-22 23:57:01 +0200

    register models to admin

--- reservation/admin.py
+++ reservation/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Hotel,Customer,Reservation
 
 # Register your models here.
+admin.site.register(Hotel)
+admin.site.register(Customer)
+admin.site.register(Reservation)
